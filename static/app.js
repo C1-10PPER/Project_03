084c0449d5a98f46f1c366317839fce83df6d4e8
unknown <hetalprajapati256@gmail.com>
2025-01-27 21:09:30 -0800

    Add Dropdown Add map but it's not work.

--- static/app.js
+++ static/app.js
@@ -1,1 +1,13 @@
-d3.csv("Raw_Data/Annual_Surface_Temp_Change.js");
+let myMap = L.map("map", {
+    center: [40.7128, -74.0059],
+    zoom: 11
+  });
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+// d3.csv("./Raw_Data/Annual_Surface_Temp_Change.csv").then((data) => {
+//     console.log(data);
+//     L.geoJson(data).addTo(myMap);
+// });
