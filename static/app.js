df24ee425ab551afaf51f25075c5bc2e2ed80d98
C1-10P <casey_wright@outlook.com>
2025-01-28 16:17:14 -0800

    Made some slight changes to the base code for the tile layer and map object

--- static/app.js
+++ static/app.js
@@ -1,10 +1,27 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+// Created a street tile layer.
+let street = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+})
+// Created a satellite tile layer as a second background of the map
+// let satellite = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+//     attribution: '© OpenStreetMap contributors, Humanitarian OpenStreetMap Team'
+//   });
 
+// Created the map object with center and zoom options.
+var map = L.map('map', {
+    center: [20, 0],
+    zoom: 2,
+    layers: [street]
+});
+
+street.addTo(map);
+
+let baseMaps = {
+    "Street": street,
+    // "Satellite": satellite,
+  };
 // d3.csv("./Raw_Data/Annual_Surface_Temp_Change.csv").then((data) => {
 //     console.log(data);
 //     L.geoJson(data).addTo(myMap);
